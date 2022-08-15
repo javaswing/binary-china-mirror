db01e931aa3f7e0018d266cda3ad71b0f1ee5817
javaswing <zxdlovejava@gmail.com>
2022-08-15 10:49:09 +0800

    feat: update pkg setting and tsconfig

---
+++ src/typings/binary-mirror-config/index.d.ts
@@ -0,0 +1,29 @@
+declare module "binary-mirror-config" {
+  interface MirrorConfigOptions {
+    /**
+     * @default 'https://registry.npmmirror.com'
+     */
+    registry?: string;
+    /**
+     * @default 5
+     */
+    retryCount?: number;
+    /** @default 5000 (ms) */
+    retryTimeout?: number;
+    /**
+     * @default console
+     */
+    console?: unknown;
+  }
+
+  class MirrorConfig {
+    constructor(config: MirrorConfigOptions);
+  }
+  export const mirrors: {
+    china: {
+      ENVS: Record<string, string>;
+      [x: string]: unknown;
+    };
+    MirrorConfig: MirrorConfig;
+  };
+}
